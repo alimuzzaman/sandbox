9a0257c9e9b2128eea011a4495781e6e7edd9415
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2026-08-28 21:50:30 +0600

    Update modularity inventory for archive slice

--- tests/test_modularity.py
+++ tests/test_modularity.py
@@ -151,7 +151,7 @@ class TestModularityInventory(unittest.TestCase):
                 "cli_commands": 89,
                 "mcp_tools": 44,
                 "wildcard_imports": 20,
-                "kind_referencing_conditionals": 161,
+                "kind_referencing_conditionals": 168,
             },
         )
 
